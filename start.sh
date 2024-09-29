906b6dbdd4f2cb403dd6af5edee0273e51da3b6f
KaioLN <kaio.leonardoss@gmail.com>
2024-09-29 18:01:10 -0300

    start.sh correction

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 VIDEO_PATH="/mnt/c/Users/Kaio/workspace/jKpCutPro/videos/teste1.mp4"
-SUBTITLE_OUTPUT_DIR="/mnt/c/Users/Kaio/workspace/jKpCutPro/subtitles"
-MIN_SENTIMENT_SCORE="0.8"  # Adicionando o valor mínimo para o sentimento
+
 
 # Passando todos os parâmetros necessários
-python3 main.py "$VIDEO_PATH" "$SUBTITLE_OUTPUT_DIR" "$MIN_SENTIMENT_SCORE"
+python3 main.py "$VIDEO_PATH"
